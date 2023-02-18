fd929b299320357891f322a64db44256e2c107ee
Anbons <anbonsuma@icloud.com>
2023-02-17 17:14:38 -0800

    putting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#!/bi/bash
 #include <stdio.h>
+
 /**
  * main - Entry point
-* Return: Always return 0 (Success)
-*/
+ * Return: Always return 0 (Success)
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
