3f117abba85656b21d98fa4c4df47a61830dfaa3
Anbons <anbonsuma@icloud.com>
2023-02-22 22:13:48 -0800

    foot

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @c: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+	return (-c);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+
