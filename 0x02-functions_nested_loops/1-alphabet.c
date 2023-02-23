234475ce549856cdfefa323150cbb52c88d8c9a0
Anbons <anbonsuma@icloud.com>
2023-02-22 22:01:18 -0800

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet in lower case
+  * Return: 0 (Success)
+*/
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);c++;
+	}
+	_putchar('\n');
+}
