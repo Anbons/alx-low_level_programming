52011a31f2493e901461c44fe96ef2ebb9e62ff9
Anbons <anbonsuma@icloud.com>
2023-02-24 18:01:33 -0800

    alalpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,15 @@
  * print_alphabet - print the alphabet in lower case
   * Return: 0 (Success)
 */
+
 void print_alphabet(void)
 {
 	char c = 'a';
 	while (c <= 'z')
 	{
-		_putchar(c);c++;
+		_putchar(c);
+		c++;
 	}
+
 	_putchar('\n');
 }
