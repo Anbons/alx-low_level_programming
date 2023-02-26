fa3d0d88fedbed2a308a1fcf385450ae4930ad44
Anbons <anbonsuma@icloud.com>
2023-02-26 07:21:51 -0800

    tn15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,6 +7,7 @@
  * Return: Always 0.
  */
 int main(void)
+
 {
 	long
 	int n1, n2, fn, afn;
@@ -14,6 +15,7 @@ int main(void)
 	n2 = 2;
 	fn = afn = 0;
 	while (fn <= 4000000)
+	
 	{
 		fn = n1 + n2;
 		n1 = n2;
@@ -23,6 +25,7 @@ int main(void)
 			afn += n1;
 		}
 	}
+	
 	printf("%ld\n", afn);
 	return (0);
 }
