72eb67294a6e446855635565b2107c39a4437b32
Anbons <anbonsuma@icloud.com>
2023-02-26 02:31:00 -0800

    tn10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,7 @@
 void times_table(void)
 {
 	int i = 0, j, k;
+
 	while (i < 10)
 	{
 		j = 0;
@@ -17,19 +18,21 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
+
 			if (k < 10 && j != 0)
 			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
 			}
+
 			else if (k >= 10)
 			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 			j++;
 		}
