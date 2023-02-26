db4af8e24dff83a31777c958db3c5fe99a678688
Anbons <anbonsuma@icloud.com>
2023-02-26 02:21:39 -0800

    tn08

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_last_digit - prints the last digit of a number
  * @c: the int to extract the last digit from
@@ -7,9 +8,12 @@
 int print_last_digit(int c)
 {
 	int a;
+
 	if (c < 0)
-	c = c;
+		c = c;
+
 	a = c % 10;
+
 	if (a < 0)
 		a = -a;
 	_putchar(a + '0');
