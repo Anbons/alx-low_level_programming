830da4b468028db28fe0a4a3bbb7053d8d80f184
Anbons <anbonsuma@icloud.com>
2023-02-26 08:13:50 -0800

    tn16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,8 @@
 int main(void)
 {
 	int c, boolean, boolean2;
-	long int n1, n2, fn, fn2, n11, n22;
+	long
+	int n1, n2, fn, fn2, n11, n22;
 	n1 = 1;
 	n2 = 2;
 	boolean =  boolean2 = 1;
@@ -27,11 +28,11 @@ int main(void)
 	{
 		If (boolean2)
 		{
-			n11 = n1 % 1000000000;
-			n22 = n2 % 1000000000;
-			n1 = n1 / 1000000000;
-			n2 = n2 / 1000000000;
-			boolean2 = 0;
+		n11 = n1 % 1000000000;
+		n22 = n2 % 1000000000;
+		n1 = n1 / 1000000000;
+		n2 = n2 / 1000000000;
+		boolean2 = 0;
 		}
 		fn2 = (n11 + n22);
 		fn = n1 + n2 + (fn2 / 1000000000);
@@ -43,9 +44,8 @@ int main(void)
 		n22 = (fn2 % 1000000000);
 	}
 	if (((n1 + n2) < 0) && boolean == 1)
-		boolean = 0;
+	boolean = 0;
 	}
 	printf("\n");
 	return (0);
-
 }           
