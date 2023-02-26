7fbe21b4396c964da452451a9ea7add2a0c5b624
Anbons <anbonsuma@icloud.com>
2023-02-26 10:48:54 -0800

    tn16

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 
 /**
  * print_alphabet - prints all alphabets in lowercase.
- * 
  * Return: On success 1.
  */
 void print_alphabet(void)
 {
 	char i;
 	for (i = 'a'; i <= 'z'; i++)
+	
 	{
 	_putchar(i);
 	}
