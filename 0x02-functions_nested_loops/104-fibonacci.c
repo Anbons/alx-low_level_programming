1ad2a19be7786e913ce9adc98584e7988bfc1376
Anbons <anbonsuma@icloud.com>
2023-02-26 14:15:27 -0800

    tn17

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,10 +21,10 @@ int main(void)
 		{
 			fn = n1 + n2;
 			printf(", %ld", fn);
-			n1 = n2 =fn;
+			n1 = n2 = fn;
 		}
 		else
-		{	
+		{
 			if (boolean2)
 			{
 			n11 = n1 % 1000000000;
@@ -33,14 +33,6 @@ int main(void)
 			n2 = n2 / 1000000000;
 			boolean2 = 0;
 			}
-			fn2 = (n11 + n22);
-			fn = n1 + n2 + (fn2 / 1000000000);
-			printf(", %ld", fn);
-			printf("%ld", fn2 % 1000000000);
-			n1 = n2;
-			n11 = n22;
-			n2 = fn;
-			n22 = (fn2 % 1000000000);
 		}
 		if (((n1 + n2) < 0) && boolean == 1)
 			boolean = 0;
