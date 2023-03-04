b89aa28a366cb432c63fa9c6455521951f04204c
Anbons <anbonsuma@icloud.com>
2023-03-03 17:00:32 -0800

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,30 +3,39 @@
 /**
  * print_number - prints an integer.
  * @n: input integer.
- * Return: no return.
+ * Return: nil.
  */
+
 void print_number(int n)
 {
-	unsigned int m, d, count;
+
+	int int_len, index;
+
+	int_len = 1000000000;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
+		_putchar('-');
+
+	if (n == -2147483648)
+	{
+		index = 1;
+		n += 1;
+	}
+	n = -n;
+	}
+	while (int_len != 1)
+	{
+		if (n >= int_len)
+			_putchar((n / int_len) % 10 + '0');
+		int_len /= 10;
+	}
+	if (index == 1)
+	{
+		_putchar(((n % 10) + 1) + '0');
 	}
 	else
 	{
-		m = n;
-		d = m;
-		count = 1;
-	}
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
+		_putchar(n % 10 + '0');
 	}
 }
