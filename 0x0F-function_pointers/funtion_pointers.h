3d4f15ea258ae6c257c97a787717c3999fffd645
Anbons <anbonsuma@icloud.com>
2023-03-23 17:54:31 -0700

    function pointer

--- 0x0F-function_pointers/funtion_pointers.h
+++ 0x0F-function_pointers/funtion_pointers.h
@@ -1,6 +1,6 @@
-#ifndef FUNC_H
-#define FUNC_H
-#include <stdlib.h>
+#include <stddef.h>
+#ifndef FUNCTION_H
+#define FUNCTION_H
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
